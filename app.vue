e01720ca23bb15326cd5c802a277443e5c3cbc1f
Isael Gomes <otakustudio188@gmail.com>
2024-01-23 23:50:32 -0300

    initialize pixi in nuxt without vue3-pixi-nuxt

--- app.vue
+++ app.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    <Application :width="240" :height="240">
-      <text :anchor="0.5" :x="120" :y="120" :style="{ fill: 'white' }">
-        Hello World
-      </text>
-    </Application>
+  <div id="game_bomb" class="h-[100vh] w-screen bg-zinc-900">
   </div>
 </template>
 
 
 <script setup lang="ts">
-import {Application} from 'vue3-pixi';
+import * as PIXI from 'pixi.js';
+import { useWindowSize } from '@vueuse/core'
+
+let app: PIXI.Application<HTMLCanvasElement>
+
+const loaderAssets = async () => {
+  const texture = await PIXI.Assets.load('https://png.pngtree.com/png-clipart/20230407/original/pngtree-cute-school-anime-chibi-character-png-image_9035249.png');
+  const character = new PIXI.Sprite(texture);
+  character.x = app.screen.width / 2;
+  character.y = app.screen.height / 2;
+  character.height = 128
+  character.width = 128
+
+// Rotate around the center
+  character.anchor.x = 0.5;
+  character.anchor.y = 0.5;
+
+  app.stage.addChild(character);
+}
 
 
+const loaderGame = async () => {
+  // Add the view to the DOM
+  const element: HTMLElement = document.querySelector("#game_bomb") as HTMLElement
+  element.appendChild(app.view)
+}
+
+onMounted(async () => {
+  // Create the application
+  app = new PIXI.Application<HTMLCanvasElement>({
+    // resizeTo: document.getElementById('game_bomb') as HTMLElement,
+    height: 360,
+    width: 640
+  })
+  await loaderGame()
+  await loaderAssets()
+})
 </script>
