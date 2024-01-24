304c0e70dfc9bad5aef210cc4634b34611d07f2f
Isael Gomes <otakustudio188@gmail.com>
2024-01-23 22:09:44 -0300

    dependencies initials

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Application :width="240" :height="240">
+      <text :anchor="0.5" :x="120" :y="120" :style="{ fill: 'white' }">
+        Hello World
+      </text>
+    </Application>
   </div>
 </template>
+
+
+<script setup lang="ts">
+import {Application} from 'vue3-pixi';
+
+
+</script>
